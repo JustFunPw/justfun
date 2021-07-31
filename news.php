ed25fded1f3fa03e2bc082f569eb6f2cf689f1e6
JustFunPw <88219365+JustFunPw@users.noreply.github.com>
2021-07-30 22:47:01 -0400

    Add files via upload

---
+++ news.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+		<title>Client Announcement</title>
+		<style type="text/css">
+			BODY {
+				/*background-image: url(/img/launcher-bg.jpg);
+				background-attachment: fixed;*/
+				background:#9BB9ED;
+				font: normal 12px colibri, tahoma, verdana, arial;
+				scrollbar-base-color: #9bb9ed;
+				/*overflow: hidden;*/
+			}
+			H1 {
+				font: bold 12px tahoma;
+				margin: 0 0 20px 0;
+			}
+			A:hover {
+				color: #0000ff;
+				text-decoration: none;
+			}
+			P {
+				margin: 0 0 20px 0;
+			}
+		</style>
+	</head>
+	
+	<body text="#000000" link="#000080" vlink="#000080" alink="#0000ff"><p align="center">Новый ларец&nbsp;<font color="#FF0000" size="2">Изумрудное сокровище</font>
+</p><br>
+<br>С радостью сообщаем, что</p>
+<p>в игровом магазине появился новый</p>
+<p>сундучок с уникальными предметами,</p>
+<p>среди которых трактат Император,</p>
+<p>Карты Сумеречной экипировки,</p>
+<p>красивые стили и невероятно сильный</p>
+<p>джинн&nbsp;<font color="#FF0000">Душа Тай Шанг!</font></p>
+<br>
+<p align="center"><font color="#ff8d1a" size="2">Желаем приятной игры!</font><br></p>
+<p align="center"><font color="#ff8d1a" size="2">Ждем вас в наших сообществах</font><br></p>
+<p align="center"><font color="#ff8d1a" size="2">в Telegram и ВКонтакте!</font><br></p></body>
+</html>
